10f5b0950b47a2aa63a026db4ab1721199327ea7
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-07-01 12:38:13 +0530

    blank files for house listing

---
+++ app/src/main/java/com/pavanmallela/wannamove/HouseListing.java
@@ -0,0 +1,13 @@
+package com.pavanmallela.wannamove;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HouseListing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_house_listing);
+    }
+}
