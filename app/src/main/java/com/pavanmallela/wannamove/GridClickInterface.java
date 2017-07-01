7da461b3db83988d46e7f0122260c33cc176ee5b
Agilan A <agilan.a@zohocorp.com>
2017-07-01 13:15:53 +0530

    cheking in main ui code

---
+++ app/src/main/java/com/pavanmallela/wannamove/GridClickInterface.java
@@ -0,0 +1,15 @@
+package com.pavanmallela.wannamove;
+
+/**
+ * Created by agilan-3269 on 01/07/17.
+ */
+
+interface GridClickInterface {
+
+    void onFindHouseClick();
+
+    void onHirePAndM();
+
+    void onHireRentalServices();
+
+}
--- app/src/main/java/com/pavanmallela/wannamove/MainActivity.java
+++ app/src/main/java/com/pavanmallela/wannamove/MainActivity.java
@@ -1,13 +1,43 @@
 package com.pavanmallela.wannamove;
 
+import android.databinding.DataBindingUtil;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.pavanmallela.wannamove.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements GridClickInterface{
+
+    private static final String TAG=MainActivity.class.getName();
+
+    GridViewAdapter gridViewAdapter;
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        gridViewAdapter=new GridViewAdapter(this);
+        binding.gridView.setAdapter(gridViewAdapter);
+
+    }
+
+    @Override
+    public void onFindHouseClick() {
+        Log.d(TAG,"onFindHouseClick");
+    }
+
+    @Override
+    public void onHirePAndM() {
+        Log.d(TAG,"onHirePAndM");
+    }
+
+    @Override
+    public void onHireRentalServices() {
+        Log.d(TAG,"onHireRentalServices");
     }
 }
